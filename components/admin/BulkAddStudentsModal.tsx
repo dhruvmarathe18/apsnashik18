69d81456c9f57319ab7cc204159824cca72ac0e1
Dhruv Marathe <dhruvmarathe18@gmail.com>
2026-01-21 01:51:26 +0530

    Add folderrrrrrr

--- components/admin/BulkAddStudentsModal.tsx
+++ components/admin/BulkAddStudentsModal.tsx
@@ -397,11 +397,11 @@ export default function BulkAddStudentsModal({ isOpen, onClose }: BulkAddStudent
                     className="hidden"
                     id="file-upload"
                   />
-                  <label htmlFor="file-upload">
-                    <Button as="span" className="cursor-pointer">
+                  <label htmlFor="file-upload" className="cursor-pointer">
+                    <span className="inline-flex items-center px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">
                       <Upload className="w-5 h-5 mr-2" />
                       Choose File
-                    </Button>
+                    </span>
                   </label>
                 </div>
               </div>
